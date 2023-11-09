9bfcae7c89564ccaca28075648d1135c508d61cd
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-11-09 17:04:02 +0000

    Agrega explicación al entrenamiento de dicción

--- BlaBlaBla/src/components/DictionTraining.tsx
+++ BlaBlaBla/src/components/DictionTraining.tsx
@@ -11,10 +11,19 @@ const DictionTraining: React.FC = () => {
 
   return (
     <View>
-      <Text>¡Mejora tu dicción!</Text>
-      <TouchableOpacity onPress={startTraining}>
-        <Text>Iniciar entrenamiento</Text>
-      </TouchableOpacity>
+        <Text>¡Mejora tu dicción!</Text>
+
+        <Text>El ejercicio es simple...</Text>
+
+        <Text>Debes ponerte un lapiz en la boca, y leer los trabalenguas.</Text> 
+
+        <Text>Al completar el ejercicio, se te mostrará la puntuación que alcanzaste, para que lo puedas mejorar.</Text>
+
+        <Text>Este simple, pero útil ejercicio, te ayudará a lograr mayor fluidez a la hora de hablar. Además, notarás tu lengua y musculos de la boca mucho más sueltos.</Text>
+
+        <TouchableOpacity onPress={startTraining}>
+            <Text>Iniciar entrenamiento</Text>
+        </TouchableOpacity>
     </View>
   );
 };
