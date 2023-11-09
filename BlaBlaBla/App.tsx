cefd3a36bbc2a4523c1ce5bf3542fb3897b23daf
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-11-09 17:28:36 +0000

    Agrega lógica de Training a DictionTraing

--- BlaBlaBla/App.tsx
+++ BlaBlaBla/App.tsx
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import DictionTraining from './src/components/DictionTraining'
-import Training from './src/components/Training'
 
 export default function App() {
   return (
--- BlaBlaBla/src/components/DictionTraining.tsx
+++ BlaBlaBla/src/components/DictionTraining.tsx
@@ -1,14 +1,41 @@
 // DictionTraining.tsx
 
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
-import Training from './Training'
 
 const DictionTraining: React.FC = () => {
+    const [tongueTwister, setTongueTwister] = useState<string>("Tres tristes tigres tragan trigo en un trigal.");
+
+    const generateRandomTongueTwister = () => {
+        // Lista de trabajenguas (puedes agregar más)
+        const tongueTwisters = [
+        "Tres tristes tigres tragan trigo en un trigal.",
+        "Pedro Pérez pintor pinta preciosos paisajes por poco precio.",
+        "Cómo poco coco como, poco coco compro.",
+        ];
+
+
+        // Seleccionar un trabalenguas aleatorio
+        const randomIndex = Math.floor(Math.random() * tongueTwisters.length);
+        const randomTongueTwister = tongueTwisters[randomIndex];
+
+        // Mostrar el trabalenguas
+        setTongueTwister(randomTongueTwister);
+    };
+
+  const startTraining = () => {
+    // Lógica para iniciar el entrenamiento
+    console.log('Entrenamiento iniciado con el trabalenguas:', tongueTwister);
+  };
   return (
     <View>
         <Text>¡Ponte un lapíz en la boca, y lee!</Text> 
-        <Training />
+        {/* Mostrar el trabalenguas actual */}
+        <Text>{tongueTwister}</Text>
+        {/* Botón para generar un nuevo trabalenguas */}
+        <TouchableOpacity onPress={generateRandomTongueTwister}>
+        <Text>Generar nuevo trabalenguas</Text>
+        </TouchableOpacity>
     </View>
   );
 };
